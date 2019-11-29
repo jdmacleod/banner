38815412039cc9bb3cdbe258c3f96db0b1776109
Jason MacLeod <jason.d.macleod@gmail.com>
2019-11-29 11:45:28 -0800

    Initial Commit

---
+++ banner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  banner
+//
+//  Created by Jason MacLeod on 11/29/19.
+//  Copyright © 2019 Jason MacLeod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
